a5e9fd5ff1cdfb2d67eaff657c2bdd9f7706f72b
github ToshiyukiTakahashi0915 <127668243+ToshiyukiTakahashi0915@users.noreply.github.com>
2023-10-21 06:40:58 +0900

    Button Styling

--- Udemy/MemoApp/src/app/index.tsx
+++ Udemy/MemoApp/src/app/index.tsx
@@ -44,8 +44,8 @@ const Index = (): JSX.Element => {
 
         </View>
 
-        <View>
-          <Text>+</Text>
+        <View style={styles.cicreButton}>
+          <Text style={styles.cicreButtonLabel}>+</Text>
         </View>
     </View>
   )
@@ -95,7 +95,29 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
     color: '#848484'
+  },
+  cicreButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#467FD3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8
+  },
+  cicreButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 48
   }
+
 })
 
 export default Index
